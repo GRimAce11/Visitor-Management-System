a64551f7a220d3557aa6efc4600f26cfbf5064d8
Chethan Nayak <82894116+GRimAce11@users.noreply.github.com>
2024-05-04 19:05:17 +0530

    Update db.firebase.js

--- src/config/db.firebase.js
+++ src/config/db.firebase.js
@@ -6,13 +6,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD8XWRly6wqbbXbccaiDdd2hRmkti-XtiA",
-    authDomain: "synce-visitor-management.firebaseapp.com",
-    projectId: "synce-visitor-management",
-    storageBucket: "synce-visitor-management.appspot.com",
-    messagingSenderId: "884700778988",
-    appId: "1:884700778988:web:456e5d12ce25244815b0c9",
-    measurementId: "G-CCLL2560VK"
+    // firebase config goes here
 };
 
 // Initialize Firebase
